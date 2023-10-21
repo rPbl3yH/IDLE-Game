c28d5dac44c10b559d18d67bd18a33b6ede45d00
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-10-21 16:59:43 +0500

    feat: add resource view

---
+++ Assets/App/Gameplay/Resource/ResourceView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace App.Gameplay.Resource
+{
+    public class ResourceView : MonoBehaviour
+    {
+        [SerializeField] private GameObject _view;
+        [SerializeField] private ResourceModel _resourceModel;
+
+        private void OnEnable()
+        {
+            _resourceModel.Amount.OnChanged += OnChanged;
+        }
+
+        private void OnDisable()
+        {
+            _resourceModel.Amount.OnChanged -= OnChanged;
+        }
+
+        private void OnChanged(int value)
+        {
+            _view.SetActive(value != 0);
+        }
+    }
+}
