62bce2e475452b413320331d3bbdc0d35c8e6fed
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-11-05 12:46:28 +0500

    feat: add build house tutorial step

---
+++ Assets/Modules/Tutorial/Content/BuildHouse_TutorialStep.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using App.Gameplay;
+using App.Gameplay.Building;
+using App.Gameplay.Building.House;
+using App.Gameplay.Resource;
+using VContainer.Unity;
+
+namespace Modules.Tutorial.Content
+{
+    public class BuildHouse_TutorialStep : IInitializable
+    {
+        private readonly TutorialState _tutorialState;
+        private readonly ResourceService _resourceService;
+        private readonly BuildingConstructionService _buildingConstructionService;
+
+        public BuildHouse_TutorialStep(
+            TutorialState tutorialState, 
+            ResourceService resourceService, 
+            BuildingConstructionService buildingConstructionService)
+        {
+            _tutorialState = tutorialState;
+            _resourceService = resourceService;
+            _buildingConstructionService = buildingConstructionService;
+        }
+
+        public void Initialize()
+        {
+            _tutorialState.StepStarted += TutorialStateOnStepStarted;
+        }
+
+        private void TutorialStateOnStepStarted(TutorialStep tutorialStep)
+        {
+            if (tutorialStep != TutorialStep.BuildHouse)
+            {
+                return;
+            }
+            
+            _resourceService.SetActiveResourceType(ResourceType.Stone, true);
+            
+            var model = _buildingConstructionService.GetServices()
+                .FirstOrDefault(model => model.BuildingModel is HouseBuildingModel);
+                
+            if (model != null)
+            {
+                model.IsEnable.Value = true;
+                model.Built.AddListener(OnBuilt);
+            }
+        }
+
+        private void OnBuilt(BuildingModel obj)
+        {
+            _tutorialState.NextStep();
+        }
+    }
+}
