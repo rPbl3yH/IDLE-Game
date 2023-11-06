00d53ef307aaa53a20f69ace3ba5a08623b6e718
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-11-06 09:33:19 +0500

    feat: add switch scene in editor

--- Assets/App/Core/SceneLoader.cs
+++ Assets/App/Core/SceneLoader.cs
@@ -1,14 +1,36 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
 namespace App.Core
 {
     public class SceneLoader
     {
-        public const string GameSceneName = "GameScene";
-
+        private const string GAME_SCENE_NAME = "GameScene";
+        private const string LOADING_SCENE_NAME = "LoadingScene";
+        private const string SCENE_EXTENSION = ".unity";
+        private const string SCENE_PATH = "Assets/App/Scenes/";
+        
         public void LoadGameScene()
         {
-            SceneManager.LoadScene(GameSceneName);
+            SceneManager.LoadScene(GAME_SCENE_NAME);
+        }
+
+        [MenuItem("Game/Open Loading Scene")]
+        public static void OpenLoadingScene()
+        {
+            OpenScene(LOADING_SCENE_NAME);
+        }
+        
+        [MenuItem("Game/Open Game Scene")]
+        public static void OpenGameScene()
+        {
+            OpenScene(GAME_SCENE_NAME);
+        }
+
+        private static void OpenScene(string name)
+        {
+            EditorSceneManager.OpenScene(SCENE_PATH + name + SCENE_EXTENSION);
         }
     }
 }
