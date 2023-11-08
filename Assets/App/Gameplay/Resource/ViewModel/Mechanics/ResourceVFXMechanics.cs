c5f34f5c59f9239c6fcaa6bdc7f9ee61e48c5003
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-11-08 07:56:59 +0500

    feat: add vfx to resources

---
+++ Assets/App/Gameplay/Resource/ViewModel/Mechanics/ResourceVFXMechanics.cs
@@ -0,0 +1,32 @@
+﻿using Modules.Atomic.Actions;
+using UnityEngine;
+
+namespace App.Gameplay.Resource.View
+{
+    public class ResourceVFXMechanics
+    {
+        private ParticleSystem _vfx;
+        private AtomicEvent<int> _gathered;
+
+        public ResourceVFXMechanics(ParticleSystem vfx, AtomicEvent<int> gathered)
+        {
+            _vfx = vfx;
+            _gathered = gathered;
+        }
+
+        public void OnEnable()
+        {
+            _gathered.AddListener(OnGathered);
+        }
+
+        private void OnGathered(int value)
+        {
+            _vfx.Play();
+        }
+
+        public void OnDisable()
+        {
+            
+        }
+    }
+}
--- Assets/App/Gameplay/Resource/ViewModel/ResourceViewModel.cs
+++ Assets/App/Gameplay/Resource/ViewModel/ResourceViewModel.cs
@@ -7,29 +7,34 @@ namespace App.Gameplay.Resource.View
     public class ResourceViewModel : MonoBehaviour
     {
         [SerializeField] private GameObject _view;
+        [SerializeField] private ParticleSystem _gatheringVfx;
         [SerializeField] private ResourceModel _resourceModel;
 
         public AtomicVariable<float> ActivateShowTime;
 
         private ActivateModelViewMechanics _activateModelViewMechanics;
         private ShakeViewMechanics _shakeViewMechanics;
+        private ResourceVFXMechanics _resourceVFXMechanics;
 
         private void Awake()
         {
             _shakeViewMechanics = new ShakeViewMechanics(_view.transform, _resourceModel.Gathered);
             _activateModelViewMechanics = new ActivateModelViewMechanics(_view, _resourceModel.IsEnable, ActivateShowTime);
+            _resourceVFXMechanics = new ResourceVFXMechanics(_gatheringVfx, _resourceModel.Gathered);
         }
 
         private void OnEnable()
         {
             _shakeViewMechanics.OnEnable();
             _activateModelViewMechanics.OnEnable();
+            _resourceVFXMechanics.OnEnable();
         }
 
         private void OnDisable()
         {
             _shakeViewMechanics.OnDisable();
             _activateModelViewMechanics.OnDisable();
+            _resourceVFXMechanics.OnDisable();
         }
     }
 }
